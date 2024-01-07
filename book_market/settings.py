c67401b75f2fae0c0de3a284098a9ebe3cd0770e
Vitalii-Chepurnyi Barry <chepurnyibarry@gmail.com>
2024-01-07 15:45:02 +0000

    Created secret key

--- book_market/settings.py
+++ book_market/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--!p^t!)8vkdn0inv8r1j#g&um8*tui&0rtta!kp%n8!q1*7!cg'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
