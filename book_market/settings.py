70f99aa2f7cd06e8fe485a9bca3aaa69d0abcc03
Vitalii-Chepurnyi Barry <chepurnyibarry@gmail.com>
2024-01-07 19:41:33 +0000

    Fixed bug

--- book_market/settings.py
+++ book_market/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-vitaliichepurny-bookhub-lxrkn8ht15p.ws-eu107.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-vitaliichepurny-bookhub-4xn1mecv3xq.ws-eu107.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
