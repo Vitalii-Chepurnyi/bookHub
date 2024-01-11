19c2134082310d192977a294599d442206331310
Vitalii-Chepurnyi Barry <chepurnyibarry@gmail.com>
2024-01-11 00:45:54 +0000

    Created new listing html file, connected it to index.html, basic html added to listing

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.MarketList.as_view(), name='home'),
+    path('<slug:slug>/', views.listing_detail, name='listing_detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -8,3 +8,13 @@ class MarketList(generic.ListView):
     queryset = Listing.objects.filter(status=1)
     template_name = "home/index.html"
     paginate_by = 3
+
+def listing_detail(request, slug):
+    queryset = Listing.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "home/listing_detail.html",
+        {"post": post},
+    )
