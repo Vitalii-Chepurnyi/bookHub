cf55f85f98357c0aa431c86383928135c663c4a2
Vitalii-Chepurnyi Barry <chepurnyibarry@gmail.com>
2024-01-12 00:06:43 +0000

    Add comment section connected to data

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Listing
+from .models import Listing, Comments
+
 
 # Create your views here.
 
@@ -12,9 +13,13 @@ class MarketList(generic.ListView):
 def listing_detail(request, slug):
     queryset = Listing.objects.filter(status=1)
     listing = get_object_or_404(queryset, slug=slug)
+    comments = listing.comments.all().order_by("-created_on")
+    comment_count = listing.comments.filter(approved=True).count()
 
     return render(
         request,
         "home/listing_detail.html",
-        {"listing": listing},
+        {"listing": listing,
+         "comments": comments,
+         "comment_count": comment_count,},
     )
